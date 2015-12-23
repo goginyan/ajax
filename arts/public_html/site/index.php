1862a2a02548ff0caa06e2f47be8a88d590ace43
goginyan <a.goginyan@mail.ru>
2015-12-24 00:42:08 +0400

    Initial commit

---
+++ arts/public_html/site/index.php
@@ -0,0 +1,24 @@
+ <?php
+
+
+
+include __DIR__ .'/header.php';
+
+//echo '<div id="contents">';
+
+include __DIR__ .'/sidebar.php';
+
+
+
+
+//include __DIR__ .'/sidebar.php';
+echo '<div id="body">';
+include __DIR__ .'/slider.php';
+
+include __DIR__ .'/body.php';
+
+echo '</div>';
+
+include __DIR__ .'/footer.php';
+
+
